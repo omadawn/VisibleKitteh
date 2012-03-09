a8ff2c3c49136c189fde06c800e8d1e463dcb005
Aaron Forster <Aaron Forster>
2012-03-08 18:07:39 -0800

    Initial Commit

---
+++ VisibleKitteh/src/org/opencv/myOpenCVFromTut1Bas1AddOpenCV/Sample1View.java
@@ -0,0 +1,152 @@
+package org.opencv.VisibleKitteh;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.LinkedList;
+import java.util.List;
+
+import org.opencv.android.Utils;
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.core.Point;
+import org.opencv.core.Rect;
+import org.opencv.core.Scalar;
+import org.opencv.core.CvType;
+import org.opencv.core.Size;
+import org.opencv.imgproc.Imgproc;
+import org.opencv.objdetect.CascadeClassifier;
+//import org.opencv.samples.fd.FdActivity;
+//import org.opencv.samples.fd.R;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.util.Log;
+import android.view.SurfaceHolder;
+
+class Sample1View extends SampleViewBase {
+    private static final String TAG = "VisibleKitteh";
+    private Mat mYuv;
+    private Mat mRgba;
+    private Mat mGraySubmat;
+    private Mat mIntermediateMat;
+    public static float         minFaceSize = 0.5f;
+    
+    private CascadeClassifier   mCascade;
+
+    public Sample1View(Context context) {
+        super(context);
+        try {
+        
+            InputStream is = context.getResources().openRawResource(R.raw.cascade);
+            File cascadeDir = context.getDir("cascade", Context.MODE_PRIVATE);
+            File cascadeFile = new File(cascadeDir, "cascade.xml");
+            FileOutputStream os = new FileOutputStream(cascadeFile);
+
+            byte[] buffer = new byte[4096];
+            int bytesRead;
+            while ((bytesRead = is.read(buffer)) != -1) {
+                os.write(buffer, 0, bytesRead);
+            }
+            is.close();
+            os.close();
+
+            mCascade = new CascadeClassifier(cascadeFile.getAbsolutePath());
+            if (mCascade.empty()) {
+                Log.e(TAG, "Failed to load cascade classifier");
+                mCascade = null;
+            } else
+                Log.i(TAG, "Loaded cascade classifier from " + cascadeFile.getAbsolutePath());
+
+            cascadeFile.delete();
+            cascadeDir.delete();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            Log.e(TAG, "Failed to load cascade. Exception thrown: " + e);
+        }
+        
+        
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder _holder, int format, int width, int height) {
+        super.surfaceChanged(_holder, format, width, height);
+
+        synchronized (this) {
+            // initialize Mats before usage
+            mYuv = new Mat(getFrameHeight() + getFrameHeight() / 2, getFrameWidth(), CvType.CV_8UC1);
+            mGraySubmat = mYuv.submat(0, getFrameHeight(), 0, getFrameWidth());
+
+            mRgba = new Mat();
+            mIntermediateMat = new Mat();
+        }
+    }
+
+    @Override
+    protected Bitmap processFrame(byte[] data) {
+    	// This is where I might be able to just replace the Imgproc stuff with something like 
+    	//mCascade.detectMultiScale(mGray, faces, 1.1, 2, 2 // TODO: objdetect.CV_HAAR_SCALE_IMAGE
+        //        , new Size(faceSize, faceSize));
+        mYuv.put(0, 0, data);
+
+        switch (Sample1Java.viewMode) {
+        case Sample1Java.VIEW_MODE_GRAY:
+            Imgproc.cvtColor(mGraySubmat, mRgba, Imgproc.COLOR_GRAY2RGBA, 4);
+            break;
+        case Sample1Java.VIEW_MODE_RGBA:
+            Imgproc.cvtColor(mYuv, mRgba, Imgproc.COLOR_YUV420sp2RGB, 4);
+            Core.putText(mRgba, "OpenCV + Android", new Point(10, 100), 3/* CV_FONT_HERSHEY_COMPLEX */, 2, new Scalar(255, 0, 0, 255), 3);
+            break;
+        case Sample1Java.VIEW_MODE_CANNY:
+            Imgproc.Canny(mGraySubmat, mIntermediateMat, 80, 100);
+            Imgproc.cvtColor(mIntermediateMat, mRgba, Imgproc.COLOR_GRAY2BGRA, 4);
+            break;
+        }
+        
+        if (mCascade != null) {
+            int height = getFrameWidth();
+            int faceSize = Math.round(height * minFaceSize);
+            List<Rect> faces = new LinkedList<Rect>();
+            mCascade.detectMultiScale(mGraySubmat, faces, 1.1, 2, 2 // TODO: objdetect.CV_HAAR_SCALE_IMAGE
+                    , new Size(faceSize, faceSize));
+
+            for (Rect r : faces)
+                Core.rectangle(mRgba, r.tl(), r.br(), new Scalar(0, 255, 0, 255), 3);
+        }
+
+//        Bitmap bmp = Bitmap.createBitmap(mRgba.cols(), mRgba.rows(), Bitmap.Config.ARGB_8888);
+
+
+        Bitmap bmp = Bitmap.createBitmap(getFrameWidth(), getFrameHeight(), Bitmap.Config.ARGB_8888);
+
+        if (Utils.matToBitmap(mRgba, bmp))
+            return bmp;
+
+        bmp.recycle();
+        return null;
+    }
+
+    @Override
+    public void run() {
+        super.run();
+
+        synchronized (this) {
+            // Explicitly deallocate Mats
+            if (mYuv != null)
+                mYuv.release();
+            if (mRgba != null)
+                mRgba.release();
+            if (mGraySubmat != null)
+                mGraySubmat.release();
+            if (mIntermediateMat != null)
+                mIntermediateMat.release();
+
+            mYuv = null;
+            mRgba = null;
+            mGraySubmat = null;
+            mIntermediateMat = null;
+        }
+    }
+}
